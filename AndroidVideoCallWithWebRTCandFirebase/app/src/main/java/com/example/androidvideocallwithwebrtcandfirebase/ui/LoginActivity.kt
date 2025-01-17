c5d91703bd4d804b4d96bd2cc850ecca060a802b
Gorur56 <sevim.gorur01@gmail.com>
2025-01-17 14:23:17 +0300

    settings viewBinding

--- AndroidVideoCallWithWebRTCandFirebase/app/src/main/java/com/example/androidvideocallwithwebrtcandfirebase/ui/LoginActivity.kt
+++ AndroidVideoCallWithWebRTCandFirebase/app/src/main/java/com/example/androidvideocallwithwebrtcandfirebase/ui/LoginActivity.kt
@@ -6,12 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidvideocallwithwebrtcandfirebase.R
+import com.example.androidvideocallwithwebrtcandfirebase.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
