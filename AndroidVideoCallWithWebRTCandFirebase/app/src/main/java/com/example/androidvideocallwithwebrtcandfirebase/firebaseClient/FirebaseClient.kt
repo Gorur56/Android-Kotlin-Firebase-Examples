3636f8ce67b233720cd84df5391cf1a4bd83c796
Gorur56 <sevim.gorur01@gmail.com>
2025-01-20 12:35:14 +0300

    create login in Firebase struct

--- AndroidVideoCallWithWebRTCandFirebase/app/src/main/java/com/example/androidvideocallwithwebrtcandfirebase/firebaseClient/FirebaseClient.kt
+++ AndroidVideoCallWithWebRTCandFirebase/app/src/main/java/com/example/androidvideocallwithwebrtcandfirebase/firebaseClient/FirebaseClient.kt
@@ -1,5 +1,9 @@
 package com.example.androidvideocallwithwebrtcandfirebase.firebaseClient
 
+import android.util.Log
+import com.example.androidvideocallwithwebrtcandfirebase.utils.FirebaseFieldNames.PASSWORD
+import com.example.androidvideocallwithwebrtcandfirebase.utils.FirebaseFieldNames.STATUS
+import com.example.androidvideocallwithwebrtcandfirebase.utils.UserStatus
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -11,21 +15,52 @@ class FirebaseClient @Inject constructor(
     private val dbRef: DatabaseReference,
     private val gson: Gson
 ){
+
+    private var currentUserName: String? = null
+
+    private fun setUserName(userName: String) {
+        this.currentUserName = userName
+    }
+
     fun login(userName: String, password: String, done: (Boolean, String?) -> Unit) {
         dbRef.addListenerForSingleValueEvent(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 //if the current user exists
                 if(snapshot.hasChild(userName)) {
                     //user existsi its tşme to check the password
-                    val dbPassword = snapshot.child(userName).child("password").value
+                    val dbPassword = snapshot.child(userName).child(PASSWORD).value
+                    if ( password == dbPassword) {
+                        //password is correct and sign in
+                        dbRef.child(userName).child(STATUS).setValue(UserStatus.ONLINE)
+                            .addOnCompleteListener {
+                                setUserName(userName)
+                                done(true,null)
+                            }.addOnFailureListener {
+                                done(false,"${it.message}")
+                            }
+                    } else {
+                        //password is wrong, notify user
+                        done(false, "Password is wrong")
+                    }
                 } else {
                     //user does not exist, register the user
+                    dbRef.child(userName).child(PASSWORD).setValue(password).addOnCompleteListener {
+                        dbRef.child(userName).child(STATUS).setValue(UserStatus.ONLINE)
+                            .addOnCompleteListener {
+                                setUserName(userName)
+                                done(true, null)
+                            }.addOnFailureListener {
+                                done(false, it.message)
+                            }
+                    }.addOnFailureListener {
 
+                    }
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+                Log.e("FirebaseClient", "Database operation was cancelled: ${error.message}")
+
             }
 
         })
--- AndroidVideoCallWithWebRTCandFirebase/app/src/main/java/com/example/androidvideocallwithwebrtcandfirebase/ui/LoginActivity.kt
+++ AndroidVideoCallWithWebRTCandFirebase/app/src/main/java/com/example/androidvideocallwithwebrtcandfirebase/ui/LoginActivity.kt
@@ -7,11 +7,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidvideocallwithwebrtcandfirebase.R
 import com.example.androidvideocallwithwebrtcandfirebase.databinding.ActivityLoginBinding
+import com.example.androidvideocallwithwebrtcandfirebase.repository.MainRepository
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    @Inject lateinit var mainRepository: MainRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +29,22 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        init()
     }
 
     private fun init() {
         binding.apply {
             buttonSignIn.setOnClickListener {
+                mainRepository.login(
+                    binding.edittextUsername.text.toString(), binding.edittextPassword.text.toString()
+                ){ isDone, reason ->
+                    if(!isDone) {
+                        Snackbar.make(binding.root, reason?: "Unknown error ", Snackbar.LENGTH_SHORT).show()
+                    } else {
+                        //start moving to mainActivity
 
+                    }
+                }
             }
         }
     }
